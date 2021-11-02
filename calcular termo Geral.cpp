bb9af0f9458e5c32e2037aeee5de5d52abd00781
emanueldemarao <emanueldemarao20@gmail.com>
2021-11-02 04:12:49 +0100

    Programa básico em Linguagem C para auxiliar estudantes no cálculo do termo geral de uma PA

---
+++ calcular termo Geral.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+	main(){
+		// Programa do Emanuel para calcular o termo Geral
+		int x,contador=0,f=0,an,a1,n=0,r;
+		printf("Digite (a1):");
+		scanf("%d",&a1);
+		printf("Digite (an):");
+			scanf("%d",&an);
+			printf("Digite (r):");
+			scanf("%d",&r);
+			n=((an-a1)/r)+1;
+	printf("%d",n);
+}
+
